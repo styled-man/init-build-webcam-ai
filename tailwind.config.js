bd878a14b8df6650b787dd0bb284b578e2147b4b
styled <smyvensestime@gmail.com>
2023-03-29 06:38:30 -0400

    slightly better font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ["**/*.jsx"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+        },
     },
     plugins: [],
 };
