abb778d242061d13964bea5b58e760484a4d6815
styled <smyvensestime@gmail.com>
2023-03-29 06:34:37 -0400

    fix small percentages not fitting on bar line

--- src/components/Emoji/index.jsx
+++ src/components/Emoji/index.jsx
@@ -1,7 +1,6 @@
 import emojiMap from "./iconMap";
 
 export default ({ children }) => {
-    console.log(emojiMap[children]);
     return (
         <div className="min-h-[35vh] w-full flex flex-col items-center justify-center gap-4">
             <h3 className="text-9xl">{emojiMap[children]}</h3>
