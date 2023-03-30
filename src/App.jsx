3c355ba062b86c4fd0e102a789056c44bf117a92
Jacob Schuster <jschuster8765@gmail.com>
2023-03-30 17:13:58 -0400

    fix cors

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState, useRef, useCallback } from "react";
 import Webcam from "react-webcam";
 import BarGraphLine from "./components/BarGraphLine";
 import Emoji from "./components/Emoji";
 import iconMap from "./components/Emoji/iconMap";
 
 function App() {
+    const webcamRef = useRef(null)
+
+    const processImage = useCallback(async () => {
+        console.log("processing")
+        const imageSrc = webcamRef.current.getScreenshot();
+        const res = await inference(imageSrc)
+        console.log(res)
+        setPredictions(res)
+    }, [webcamRef])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            processImage()
+        }, 3000)
+        return () => clearInterval(interval)
+    }, [])
+
     const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+    const inference = async (img) => {
+        const request = await fetch("http://localhost:8000/process", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                image: img,
+            })
+        })
+        const response = await request.json()
+        console.log("RESPONSE", response)
+        return response
+    }
 
     // initial value: an object containing all the emotions from the icon map initialized to 0
     // ex: {happy: 0, anger: 0, sad: 0, ...}
@@ -44,7 +75,9 @@ function App() {
     return (
         <div className="grid grid-cols-2 min-h-screen">
             <div className="flex items-center justify-center border-r-4">
-                <Webcam className="rounded-lg" />
+                <Webcam className="rounded-lg"
+                    ref={webcamRef}
+                />
             </div>
 
             <div>
